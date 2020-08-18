c4f6d1478d241e77757e1e9b0d1c00c3e4c743fe
erichsueh3 <hsuehe@amazon.com>
2020-08-17 18:12:57 -0700

    Changes to ExportResult in exporter interface

--- sdk/include/opentelemetry/sdk/metrics/exporter.h
+++ sdk/include/opentelemetry/sdk/metrics/exporter.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #pragma once
 
 #include <memory>
@@ -22,7 +38,22 @@ enum class ExportResult
    * Exporting failed. The caller must not retry exporting the same batch; the
    * batch must be dropped.
    */
-  kFailure
+  kFailure = 1,
+
+  /**
+   * The collection does not have enough space to receive the export batch.
+   */
+  kFailureFull = 2,
+
+  /**
+   * The export has timed out.
+   */
+  kFailureTimeout = 3,
+
+  /**
+   * The export() function was passed an invalid argument.
+   */
+  kFailureInvalidArgument = 4,
 };
 /**
  * MetricsExporter defines the interface that protocol-specific span exporters must
